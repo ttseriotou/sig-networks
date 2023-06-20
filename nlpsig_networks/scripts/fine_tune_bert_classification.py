580f7139de5748a88f031483e019db9c9e647867
rchan <rchan@turing.ac.uk>
2023-06-20 09:45:18 +0100

    first attempt at fine-tuning bert baseline

--- nlpsig_networks/scripts/fine_tune_bert_classification.py
+++ nlpsig_networks/scripts/fine_tune_bert_classification.py
@@ -65,13 +65,13 @@ def testing_transformer(
 
 
 def _fine_tune_transformer_for_indices(
+    num_epochs: int,
     pretrained_model_name: str,
     df: pd.DataFrame,
     feature_name: str,
     label_column: str,
     indices: tuple[list[int], list[int], list[int]],
     seed: int,
-    num_epochs: int,
     save_model: bool = False,
     output_dir: str | None = None
 ) -> dict[str, float | list[float]]:
@@ -92,6 +92,9 @@ def _fine_tune_transformer_for_indices(
     label_to_id = {y_data.unique()[i]: i for i in range(len(y_data.unique()))}
     id_to_label = {v: k for k, v in label_to_id.items()}
     output_dim = len(label_to_id.values())
+    
+    # create column named "label" which are the corresponding IDs
+    df["label"] = df[label_column].apply(lambda x: label_to_id[x])
     
     # initialise model, tokenizer and data_collator
     model = AutoModelForSequenceClassification.from_pretrained(
@@ -148,26 +151,28 @@ def _fine_tune_transformer_for_indices(
         feature_name=feature_name,
     )
     
-    if not save_model:
+    if save_model:
+        text_encoder.trainer.save_model(output_dir)
+    else:
+        # if do not request to save the model,
+        # make sure to delete any folders created
         if os.path.isdir(output_dir):
             os.rmdir(output_dir)
             
     return test_performance
 
 
-# TODO: finish the below
-
 def fine_tune_transformer_for_classification(
+    num_epochs: int,
     pretrained_model_name: str,
     df: pd.DataFrame,
     feature_name: str,
     label_column: str,
-    indices: tuple[list[int], list[int], list[int]],
     seed: int,
-    num_epochs: int,
     data_split_seed: int = 0,
     k_fold: bool = False,
     n_splits: int = 5,
+    return_metric_for_each_fold: bool = False,
 ):
     """
     Function to fine-tune and evaluate a model by either using k_fold
@@ -177,6 +182,7 @@ def fine_tune_transformer_for_classification(
     # set seed
     set_seed(seed)
     
+    # set y_data as the correct column
     y_data = df[label_column]
     
     if k_fold:
@@ -189,17 +195,45 @@ def fine_tune_transformer_for_classification(
                       shuffle=True,
                       random_state=data_split_seed)
         
+        accuracy = []
+        f1 = []
+        f1_scores = []
+        labels = torch.empty((0))
+        predicted = torch.empty((0))
         for k in range(n_splits):
+            # compute how well the model performs on this fold
             results_for_fold = _fine_tune_transformer_for_indices(
+                num_epochs=num_epochs,
                 pretrained_model_name=pretrained_model_name,
                 df=df,
                 feature_name=feature_name,
                 label_column=label_column,
                 indices=folds.fold_indices[k],
                 seed=seed,
-                num_epochs=num_epochs,
                 save_model=False,
             )
+            
+            # store the true labels and predicted labels for this fold
+            labels = torch.cat([labels, results_for_fold["labels"]])
+            predicted = torch.cat([predicted, results_for_fold["predicted"]])
+            
+            # store the metrics for this fold
+            accuracy.append(results_for_fold["accuracy"])
+            f1.append(results_for_fold["f1"])
+            f1_scores.append(results_for_fold["f1_scores"])
+            
+        if return_metric_for_each_fold:
+            # return how well the model performed on each individual fold
+            return pd.DataFrame({"accuracy": accuracy,
+                                 "f1": f1,
+                                 "f1_scores": f1_scores})
+        else:
+            # compute how well the model performed on the test sets together
+            # compute accuracy
+            accuracy = ((predicted == labels).sum() / len(labels)).item()
+            # compute F1
+            f1_scores = metrics.f1_score(labels, predicted, average=None)
+            f1 = sum(f1_scores)/len(f1_scores)
     else:
         # split dataset
         # x_data is just a dummy torch tensor of size (len(y_data)) to get the fold indices
@@ -210,6 +244,7 @@ def fine_tune_transformer_for_classification(
                                 shuffle=True,
                                 random_state=data_split_seed)
         
+        # compute how well the model performs on this data split
         results = _fine_tune_transformer_for_indices(
             pretrained_model_name=pretrained_model_name,
             df=df,
@@ -221,10 +256,65 @@ def fine_tune_transformer_for_classification(
             save_model=False,
         )
         
+        return pd.DataFrame({"accuracy": results["accuracy"],
+                             "f1": results["f1"],
+                             "f1_scores": [results["f1_scores"]]})
+        
 
-def fine_tune_transformer_average_seed():
+def fine_tune_transformer_average_seed(
+    num_epochs: int,
+    pretrained_model_name: str,
+    df: pd.DataFrame,
+    feature_name: str,
+    label_column: str,
+    seeds: list[int],
+    data_split_seed: int = 0,
+    k_fold: bool = False,
+    n_splits: int = 5,
+    validation_metric: str = "f1",
+    return_metric_for_each_fold: bool = False,
+    results_output: str | None = None,
+    verbose: bool = True
+):
     """
     Function to fine-tune and evaluate a model (using k-fold or standard dataset split)
     for various seeds and average over performance
     """
-    pass
+    if validation_metric not in ["accuracy", "f1"]:
+        raise ValueError("validation_metric must be either 'accuracy' or 'f1'")
+    
+    test_scores = []
+    test_results_df = pd.DataFrame()
+    for seed in seeds:
+        test_results = fine_tune_transformer_for_classification(
+            num_epochs=num_epochs,
+            pretrained_model_name=pretrained_model_name,
+            df=df,
+            feature_name=feature_name,
+            label_column=label_column,
+            seed=seed,
+            data_split_seed=data_split_seed,
+            k_fold=k_fold,
+            n_splits=n_splits,
+            return_metric_for_each_fold=return_metric_for_each_fold
+        )
+        
+        test_results["seed"] = seed
+        test_results["k_fold"] = k_fold
+        test_results_df = pd.concat([test_results_df, test_results])
+        
+        # save metric that we want to validate on
+        # taking the mean over the performance on the folds for the seed
+        # if k_fold=False, .mean() just returns the performance for the seed
+        test_scores.append(test_results[validation_metric].mean())
+        
+    test_scores_mean = sum(test_scores)/len(test_scores)
+    if verbose:
+        print(f"- average (test) metric score: {test_scores_mean}")
+        print(f"scores for the different seeds: {test_scores}")
+        
+    if results_output is not None:
+        print(f"saving the results dataframe to CSV in {results_output}")
+        test_results_df.to_csv(results_output)
+
+    return test_results_df
