ff230bb8496a13677294a703899b7964e40e6410
rchan <rchan@turing.ac.uk>
2023-08-01 07:48:24 +0100

    client runs and therapist start

--- notebooks/AnnoMI/load_anno_mi.py
+++ notebooks/AnnoMI/load_anno_mi.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import torch
 
 
 ##################################################
@@ -30,6 +31,8 @@ id_to_label_client = {v: k for k, v in label_to_id_client.items()}
 y_data_client = [label_to_id_client[x] for x in y_data_client]
 # output dimension for client talk type
 output_dim_client = len(label_to_id_client.keys())
+# obtain the transcript ids for the client
+client_transcript_id = torch.tensor(anno_mi["transcript_id"][client_index].values)
 
 ##################################################
 ########## Main therapist behaviour ##############
@@ -47,3 +50,5 @@ id_to_label_therapist = {v: k for k, v in label_to_id_therapist.items()}
 y_data_therapist = [label_to_id_therapist[x] for x in y_data_therapist]
 # output dimension for therapist behaviour
 output_dim_therapist= len(label_to_id_therapist.keys())
+# obtain the therapist ids for the client
+therapist_transcript_id = torch.tensor(anno_mi["transcript_id"][therapist_index].values)
