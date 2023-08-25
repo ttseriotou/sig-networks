252c4ba8accb707a3a516986760ae7c610fd6de3
rchan <rchan@turing.ac.uk>
2023-08-25 22:05:59 +0100

    update snwu tensor convert

--- nlpsig_networks/scripts/swnu_network_functions.py
+++ nlpsig_networks/scripts/swnu_network_functions.py
@@ -133,8 +133,18 @@ def implement_swnu_network(
         print(swnu_network_model)
     
     # convert data to torch tensors
-    if not isinstance(x_data, torch.Tensor):
-        x_data = torch.tensor(x_data)
+    # deal with case if x_data is a dictionary
+    if isinstance(x_data, dict):
+        # iterate through the values and check they are of the correct type
+        for key, value in x_data.items():
+            if not isinstance(value, torch.Tensor):
+                x_data[key] = torch.tensor(value)
+            x_data[key] = x_data[key].float()
+    else:
+        # convert data to torch tensors
+        if not isinstance(x_data, torch.Tensor):
+            x_data = torch.tensor(x_data).float()
+        x_data = x_data.float()
     if not isinstance(y_data, torch.Tensor):
         y_data = torch.tensor(y_data)
 
