3a9f06e144b3eeba2a9fdefc1818e0768a1d5c51
rchan <rchan@turing.ac.uk>
2023-11-19 14:14:56 +0000

    load in certain modules

--- src/sig_networks/__init__.py
+++ src/sig_networks/__init__.py
@@ -1,3 +1,28 @@
 from __future__ import annotations
 
 __version__ = "0.1.0"
+
+from .swnu import SWLSTM, SWNU  # noqa: E402
+from .swnu_network import SWNUNetwork  # noqa: E402
+from .swmhau import SWMHA, SWMHAU  # noqa: E402
+from .swmhau_network import SWMHAUNetwork  # noqa: E402
+from .seqsignet_bilstm import SeqSigNet  # noqa: E402
+from .seqsignet_attention_encoder import SeqSigNetAttentionEncoder  # noqa: E402
+from .seqsignet_attention_bilstm import SeqSigNetAttentionBiLSTM  # noqa: E402
+from .scripts.swnu_network_functions import obtain_SWNUNetwork_input  # noqa: E402
+from .scripts.seqsignet_functions import obtain_SeqSigNet_input  # noqa: E402
+
+__all__ = (
+    "__version__",
+    "SWLSTM",
+    "SWNU",
+    "SWMHA",
+    "SWMHAU",
+    "SWNUNetwork",
+    "SWMHAUNetwork",
+    "SeqSigNet",
+    "SeqSigNetAttentionEncoder",
+    "SeqSigNetAttentionBiLSTM",
+    "obtain_SWNUNetwork_input",
+    "obtain_SeqSigNet_input",
+)
