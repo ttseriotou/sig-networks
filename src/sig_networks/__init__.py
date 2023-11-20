a122acba0e9c186105d5112181281846f0f24dec
rchan <rchan@turing.ac.uk>
2023-11-20 10:01:51 +0000

    bump version 0.2.0

--- src/sig_networks/__init__.py
+++ src/sig_networks/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from .swnu import SWLSTM, SWNU  # noqa: E402
 from .swnu_network import SWNUNetwork  # noqa: E402
